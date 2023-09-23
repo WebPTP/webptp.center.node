12a496e95d59a4db00fd18232694921c0c578c44
yihrmc <yihrmc@163.com>
2023-09-23 16:46:22 +0800

    Update process.env

--- cert.js
+++ cert.js
@@ -1,30 +1,30 @@
 const crypto = require('crypto');
 
-let publicKeyBase64 = process.env.publicKeyBase64 || undefined;
-let privateKeyBase64 = process.env.privateKeyBase64 || undefined;
+let publicKeyHex = process.env.publicKeyHex || undefined;
+let privateKeyHex = process.env.privateKeyHex || undefined;
 
-if (!publicKeyBase64 || !privateKeyBase64) {
+if (!publicKeyHex || !privateKeyHex) {
     // 生成RSA密钥对
     const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
         modulusLength: 2048, // 密钥长度
     });
     // 将公钥和私钥以Base64编码保存
-    publicKeyBase64 = publicKey.export({ type: 'spki', format: 'der' }).toString('hex');
-    privateKeyBase64 = privateKey.export({ type: 'pkcs8', format: 'der' }).toString('hex');
+    publicKeyHex = publicKey.export({ type: 'spki', format: 'der' }).toString('hex');
+    privateKeyHex = privateKey.export({ type: 'pkcs8', format: 'der' }).toString('hex');
     // 输出生成的秘钥
     console.log("已生成秘钥:" 
         + "\r\n - 公钥: [%s]"
-        + "\r\n - 私钥: [%s]", publicKeyBase64, privateKeyBase64);
+        + "\r\n - 私钥: [%s]", publicKeyHex, privateKeyHex);
 }
 
 // 加载Base64编码的公钥和私钥
 const publicKey = crypto.createPublicKey({
-    key: Buffer.from(publicKeyBase64, 'hex'),
+    key: Buffer.from(publicKeyHex, 'hex'),
     format: 'der',
     type: 'spki',
 });
 const privateKey = crypto.createPrivateKey({
-    key: Buffer.from(privateKeyBase64, 'hex'),
+    key: Buffer.from(privateKeyHex, 'hex'),
     format: 'der',
     type: 'pkcs8',
 });
